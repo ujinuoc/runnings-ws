a625884f265bda9fafa91acf3a8c4e2b4c4c9bd1
aruizfalco <antonio@aruizfalco.com>
2019-04-14 13:47:50 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+# Check the python version. If the python version is 2, print message and exit.
+if sys.version_info[0] < 3:
+    print ("This program requires Python 3")
+    exit()
+
+# Import scraper class
 from scraper import CarrerasScraper
 
-# Crear el scraper de carreras
+# Scraper creation
 scraper = CarrerasScraper();
 
-# Iniciar el proceso de scraping y guardar los resultados
+# Initiates scraping process and stores the results
 scraper.scrape();
